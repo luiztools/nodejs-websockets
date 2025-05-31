8107986f129550b69eb8cdda2a571730a36b2078
LuizTools <luizfduartejr@gmail.com>
2025-05-30 22:44:32 -0300

    bugfix react-use-websocket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,13 @@ import useWebSocket from 'react-use-websocket';
 function App() {
   const [numero, setNumero] = useState(0);
 
-  const { lastJsonMessage, sendMessage } = useWebSocket('ws://localhost:3001', {
+  useWebSocket('ws://localhost:3001', {
     onOpen: () => console.log(`Connected to App WS`),
-    onMessage: () => {
-      if (lastJsonMessage) {
-        console.log(lastJsonMessage);
-        setNumero(lastJsonMessage.n);
-      }
+    onMessage: (message) => {
+      if (!message) return;
+      const data = JSON.parse(message.data);
+      console.log(data);
+      setNumero(data.n);
     },
     queryParams: { 'token': '123456' },
     onError: (event) => { console.error(event); },
